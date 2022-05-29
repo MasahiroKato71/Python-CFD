2928d9dc693c161b88fdf2d07f300644aaba2975
MasahiroKato71 <106437408+MasahiroKato71@users.noreply.github.com>
2022-05-29 16:02:12 +0900

    Update: 物性値基底モデルの追加
    流束関数を追加しました

--- 1dEuler/ModelBase.py
+++ 1dEuler/ModelBase.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
+
+
 class PhysicalPropertyBase():
     def __init__(self, length:int):
         if type(length) is not int:
             raise TypeError
         
-        self.valeu = [0 for _ in range(length)]
-        self.leftBoundaryValue = [0 for _ in range(length)]
-        self.rightBoundaryValue = [0 for _ in range(length)]
+        self.value = [0 for _ in range(length)]
+        self.leftBoundaryValue = [0 for _ in range(length+1)]
+        self.rightBoundaryValue = [0 for _ in range(length+1)]
+        
+    def F(self):
+        raise NotImplementedError
+            
