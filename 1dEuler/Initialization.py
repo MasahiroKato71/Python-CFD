d435943927d703f1e491a6eb959527e39439f7e3
MasahiroKato71 <106437408+MasahiroKato71@users.noreply.github.com>
2022-05-31 12:40:31 +0900

    Add: 初期化処理関数の追加
    ショックチューブ問題の初期化処理を実装しました

---
+++ 1dEuler/Initialization.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from PhysicalProperty import *
+from Reconstruction import *
+from Solver import *
+
+
+def ShockTube(lenght:int, rhoLow:float, rhoHigh:float, pLow:float, pHigh:float, xMin:float, xMax:float, gamma:float=1.4):
+    dx = (xMax - xMin) / lenght
+    lenght += 2 # ゴーストセル分加算
+    rho = Rho(lenght)
+    rhoU = RhoU(lenght)
+    rhoE = RhoE(lenght)
+    p = P(gamma)
+    x = X(lenght)
+    
+    x.value[0] = xMin - dx
+    for i in range(1,lenght+1):
+        x.value[i] = x.value[i-1] + dx
+        
+    for i, x_ in enumerate(x.value[:-1]):
+        if x_ < (xMax+xMin)/2:
+            p_ = pHigh
+            u_ = 0.
+            rho_ = rhoHigh
+        else:
+            p_ = pLow
+            u_ = 0.
+            rho_ = rhoLow
+        rho.value[i] = rho_
+        rhoU.value[i] = rho_ * u_
+        rhoE.value[i] = p_/(gamma - 1) - .5 * u_**2 * rho_
+        
+    return rho, rhoU, rhoE, p, x
