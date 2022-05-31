2685d4a1c9b2fac2714d4de5dbed85b8b431dd32
MasahiroKato71 <106437408+MasahiroKato71@users.noreply.github.com>
2022-05-31 12:41:41 +0900

    Update: メインルーチンの更新
    クラスを利用した処理に更新しました

--- 1dEuler/Main.py
+++ 1dEuler/Main.py
@@ -1,104 +1,18 @@
 from __future__ import annotations
 
-from typing import Final
-
-import matplotlib.pyplot as plt
-
+import Initialization 
 from PhysicalProperty import *
 from Reconstruction import *
 from Solver import *
-
-
-def update(rho:Rho, rhoU:RhoU, rhoE:RhoE, x:X):
-     if not (type(rho) is Rho and type(rhoU) is RhoU and type(rhoE) is RhoE):
-          raise TypeError
-     if type(x) is not X:
-          raise TypeError
-     for i in range(1, len(rho.value)):
-          rho.value[i] += dt*(rho.f[i] - rho.f[i+1]) / (x.value[i+1] - x.value[i])
-          rhoU.value[i] += dt*(rhoU.f[i] - rhoU.f[i+1]) / (x.value[i+1] - x.value[i])
-          rhoE.value[i] += dt*(rhoE.f[i] - rhoE.f[i+1]) / (x.value[i+1] - x.value[i])
+from TimeIntegrationMethod import *
           
-          
-def cfl(rho:Rho, rhoU:RhoU, rhoE:RhoE, p:P, x:X, cflnum:float=0.5) -> float:
-     if not (type(rho) is Rho and type(rhoU) is RhoU and type(rhoE) is RhoE):
-          raise TypeError
-     if not (type(p) is P and type(x) is X and type(cflnum) is float):
-          raise TypeError
-     dt = 1e2
-     for i in range(len(rho.value)):
-          u = rhoU.value[i] / rho.value[i]
-          c = math.sqrt(p.gamma * p(rho.value[i], rhoU.value[i], rhoE.value[i])/rho.value[i])
-          lambda1 = abs(u)
-          lambda2 = abs(u + c)
-          lambda3 = abs(u - c)
-          lambdaMax = max(lambda1, lambda2, lambda3, 0.1)
-          dt = min(dt, cflnum*(x.value[i+1] - x.value[i])/lambdaMax)
-     
-     return dt
-          
-          
-LENGTH:Final[int] = 100
-XMIN:Final[float] = -1.
-XMAX:Final[float] = 1.
-TSTOP:Final[float] = .5
 
-GAMMA:Final[float] = 1.4
-CFL:Final[float] = .5
+lenght = 100
+pLow, pHigh = 0.1, 1.0
+rhoLow, rhoHigh = 0.1, 1.0
+xMin, xMax = -1.0 ,1.0
 
-pltInterval = 25
-
-rho = Rho(LENGTH)
-rhoU = RhoU(LENGTH)
-rhoE = RhoE(LENGTH)
-p = P(GAMMA)
-
-x = X(LENGTH)
-dx = (XMAX - XMIN)/LENGTH
-
-# 初期化処理
-x.value[0] = XMIN
-for i in range(1,len(x.value)):
-     x.value[i] = x.value[i-1] + dx
-     
-for i, x_ in enumerate(x.value[:-1]):
-     if x_ < 0:
-          p_ = 1.
-          u_ = 0.
-          rho_ = 1.
-     else:
-          p_ = 0.1
-          u_ = 0.
-          rho_ = 0.1
-     rho.value[i] = rho_
-     rhoU.value[i] = rho_ * u_
-     rhoE.value[i] = p_/(GAMMA - 1) - .5 * u_**2 * rho_
-               
-
-t = 0.
-n = 0.
-
+rho, rhoU, rhoE, p, x = Initialization.ShockTube(lenght, rhoLow, rhoHigh, pLow, pHigh, xMin, xMax)
 riemannRoe = RiemannRoe(rho, rhoU, rhoE, p, epsilon=0.15)
-plt.figure(figsize=(16,9))
-plt.rcParams["font.size"] = 26
-plt.plot(x.value[:-1], rho.value, lw=2, label="t=0")
-
-while t < TSTOP:
-     n += 1
-     dt = cfl(rho, rhoU, rhoE, p, x, CFL)
-     t += dt
-     Reconstuctions.piece_wise(rho)
-     Reconstuctions.piece_wise(rhoU)
-     Reconstuctions.piece_wise(rhoE)
-     riemannRoe()
-     update(rho, rhoU, rhoE, x)
-     if n % pltInterval == 0:
-          plt.plot(x.value[:-1], rho.value, lw=2, label=f"t={t:.2f}")
-
-plt.xlabel("x")
-plt.ylabel("rho")
-plt.legend()
-plt.savefig("Eulear.png")
-     
-     
-     
+eulearFront = EulearFront(rho, rhoU, rhoE, p, x, riemannRoe, Reconstuctions.piece_wise, cflnum=.5)
+eulearFront(starttime=0, endtime=0.6, figName="WindUp.png")
