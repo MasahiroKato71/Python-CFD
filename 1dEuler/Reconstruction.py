346d0be26c0d19168f86548895b50e4adcece3aa
MasahiroKato71 <106437408+MasahiroKato71@users.noreply.github.com>
2022-06-02 00:18:31 +0900

    Add: 再構築関数の追加
    TVD法を実装しました
    リミッタ関数の追加しました

--- 1dEuler/Reconstruction.py
+++ 1dEuler/Reconstruction.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
 from ModelBase import *
-
+import Utils
 
 class Reconstuctions():
+    def __init__(self, limiter:function = None):
+        if not (callable(limiter) or limiter is None):
+            raise TypeError
+        
+        self.limiter = limiter
+        
     @staticmethod
     def PieceWise(physicalProperty:PhysicalPropertyBase) -> None:
         if not isinstance(physicalProperty, PhysicalPropertyBase):
@@ -13,3 +19,47 @@ class Reconstuctions():
             physicalProperty.leftBoundaryValue[i] = physicalProperty.value[i-1]
             physicalProperty.rightBoundaryValue[i] = physicalProperty.value[i]
             
+    def TVD(self, physicalProperty:PhysicalPropertyBase) -> None:
+        if not isinstance(physicalProperty, PhysicalPropertyBase):
+            raise TypeError
+        if not callable(self.limiter):
+            raise TypeError
+        
+        for i in range(2, len(physicalProperty.leftBoundaryValue)-2):
+            physicalProperty.leftBoundaryValue[i] = physicalProperty.value[i-1] \
+                + .5 * self.limiter((physicalProperty.value[i-1] - physicalProperty.value[i-2]) / Utils.MaxAbs((physicalProperty.value[i] - physicalProperty.value[i-1]), 1e-6)) \
+                    * (physicalProperty.value[i] - physicalProperty.value[i-1])
+            
+            physicalProperty.rightBoundaryValue[i] = physicalProperty.value[i] \
+                - .5 * self.limiter((physicalProperty.value[i+1] - physicalProperty.value[i]) / Utils.MaxAbs((physicalProperty.value[i] - physicalProperty.value[i-1]), 1e-6)) \
+                    * (physicalProperty.value[i] - physicalProperty.value[i-1])
+
+
+class Limiter():
+    @staticmethod
+    def Minmod(r:float) -> float:
+        if type(r) is not float:
+            raise TypeError
+        
+        return max(0, min(1, r))
+    
+    @staticmethod
+    def Superbee(r:float) -> float:    
+        if type(r) is not float:
+            raise TypeError
+        
+        return max(0, min(1, 2*r), min(2, r))
+    
+    @staticmethod
+    def VanLeer(r:float) -> float:
+        if type(r) is not float:
+            raise TypeError
+        
+        return (r + abs(r)) / (1 + abs(r))
+    
+    @staticmethod
+    def VanAlbada(r:float) -> float:
+        if type(r) is not float:
+            raise TypeError
+        
+        return (r + r**2) / (1 + r**2)
